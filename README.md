ece5ce4db6e41caa437ed8d08849b93685671895
ksimanov <ksimanov@ronasit.com>
2016-12-01 18:44:58 +0600

    Init repo; Update version

---
+++ README.md
@@ -0,0 +1,42 @@
+# jQuery swipe event
+
+Plugin lets to use swipe touch events.
+
+# Install with bower
+```
+bower install jq-swipe-events
+```
+
+# Usage
+
+## Main swipe event
+```javascript
+$(selector).swipeEvent().on('swipe', function(e) {
+    // your actions
+});
+```
+
+## Additional events:
+
+- swipeLeft;
+- swipeRight;
+- swipeUp;
+- swipeDown.
+
+## Swipe event object
+
+Swipe event object is extended `touchmove` event. Following properties are added:
+
+- **e.swipeDeltaX**: Number,
+    positive for `swipeRight`
+    negative for `swipeLeft`
+    
+- **e.swipeDeltaY**: Number,
+    positive for `swipeDown`
+    negative for `swipeUp`
+    
+- **e.pageX**: Number
+    original `touchmove` e.pageX
+
+- **e.pageY**
+    original `touchmove` e.pageY
---
+++ jq-swipe-events.js
@@ -0,0 +1,70 @@
+;(function ($, window, document, undefined) {
+  'use strict';
+
+  var pluginName = 'swipeEvents';
+  var TOUCHSTART = 'touchstart.'  + pluginName,
+      TOUCHEND = 'touchend.'  + pluginName,
+      TOUCHMOVE = 'touchmove.'  + pluginName;
+
+  var abs = Math.abs;
+
+  $.fn.swipeEvents = swipeEvents;
+
+  function swipeEvents() {
+    return this.each(function (i, el) {
+      var startX, startY, $el = $(el);
+
+
+      $el.on(TOUCHSTART, start);
+      $el.on(TOUCHEND, end);
+
+      function start(e) {
+        if (e.touches.length === 1) e.preventDefault();
+        var touch = e.originalEvent.touches[0];
+
+        startX = touch.pageX;
+        startY = touch.pageY;
+
+        $el.on(TOUCHMOVE, move);
+      }
+
+      function move(e) {
+        var touch = e.originalEvent.touches[0];
+        var deltaX = touch.pageX - startX,
+            deltaY = touch.pageY - startY,
+            event = {
+              swipeDeltaX: -deltaX,
+              swipeDeltaY: -deltaY,
+              pageX: touch.pageX,
+              pageY: touch.pageY
+            };
+
+        if (abs(deltaY) > 0 || abs(deltaY) > 0) {
+          $el.trigger(
+            $.Event('swipe', event));
+        }
+
+        if (deltaX > 0) {
+          $el.trigger($.Event('swipeLeft', event));
+        }
+
+        if (deltaX < 0) {
+          $el.trigger($.Event('swipeRight', event));
+        }
+
+        if (deltaY > 0) {
+          $el.trigger($.Event('swipeDown', event));
+        }
+
+        if (deltaY < 0) {
+          $el.trigger($.Event('swipeUp', event));
+        }
+      }
+
+      function end(e) {
+        e.preventDefault();
+        $el.off(TOUCHMOVE, move);
+      }
+    })
+  }
+})(jQuery, window, document);
